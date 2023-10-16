e5e95b2a3a4d2514aba904c36d1d79713535bb89
AlanBall22 <120392829+AlanBall22@users.noreply.github.com>
2023-10-16 16:43:13 +0100

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+    
+<head>
+    <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Argentum+Sans+Light&display=swap" rel="stylesheet">
+
+<body style="font-family: 'Argentum Sans Light', sans-serif;">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+
+</head>
+<body>
+ 
+    
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <!-- Logo -->
+                <img src = "https://vesta.uclan.ac.uk/~dxu6/co2717/webapp/week3/images/logo.svg" alt = "Nike Logo">
+            </div>
+            <div class="col-8">            
+                <!-- Navigation -->           
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Men</a>
+                                <a class="nav-link" href="#">Women</a>
+                                <a class="nav-link" href="#">Kid</a>
+                                <a class="nav-link" href="#">Personalise</a>
+                                <a class="nav-link" href="#">Collections</a>
+                                <a class="nav-link" href="#">Sales</a>
+                            </li>
+                         </ul>
+                    </div>
+                    </div>
+                </nav>
+             <!-- Search Icon -->   
+             <i class="fa-solid fa-magnifying-glass"></i>
+                 
+         </div>
+            <div class="col-2">
+                <!-- Cart Icon -->
+                <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+        </div>
+    </div>
+    
+</header>
+
+</body>
+
+</html>
